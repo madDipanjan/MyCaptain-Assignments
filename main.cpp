8f1c854c25bd6f075522cdabd22dc0336f65493d
Dipanjan Majumdar <67099136+madDipanjan@users.noreply.github.com>
2020-08-07 22:10:03 +0530

    main.cpp modified
    using namespace std is removed from the top of the code. std is used by the :: (scope resolution) operator

--- main.cpp
+++ main.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
 
-using namespace std;
-
 int main()
 {
-    cout << "Size of char: " << sizeof(char) << " byte" << endl;
-    cout << "Size of int: " << sizeof(int) << " bytes" << endl;
-    cout << "Size of float: " << sizeof(float) << " bytes" << endl;
-    cout << "Size of double: " << sizeof(double) << " bytes" << endl;
+    std :: cout << "Size of char: " << sizeof(char) << " byte" << std :: endl;
+    std :: cout << "Size of int: " << sizeof(int) << " bytes" << std :: endl;
+    std :: cout << "Size of float: " << sizeof(float) << " bytes" << std :: endl;
+    std :: cout << "Size of double: " << sizeof(double) << " bytes" << std :: endl;
 
     return 0;
 }
