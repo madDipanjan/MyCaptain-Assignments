c3dd881f1894f22929c508301d95960468bf1cb0
Dipanjan Majumdar <67099136+madDipanjan@users.noreply.github.com>
2020-08-08 00:11:49 +0530

    Answer of Q.1 in Assiggnment1 uploded
    This code prints the size of different data types.

---
+++ data_type_size.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    std :: cout << "Size of char: " << sizeof(char) << " byte" << std :: endl;
+    std :: cout << "Size of int: " << sizeof(int) << " bytes" << std :: endl;
+    std :: cout << "Size of float: " << sizeof(float) << " bytes" << std :: endl;
+    std :: cout << "Size of double: " << sizeof(double) << " bytes" << std :: endl;
+
+    return 0;
+}
